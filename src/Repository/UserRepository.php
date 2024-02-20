878195df60d6e7bc1d4a6d212e76fc2eb92ebfd4
Nzo <73233840+Dehan667@users.noreply.github.com>
2024-02-20 21:07:08 +0100

    Add files via upload

---
+++ src/Repository/UserRepository.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\User;
+use App\Entity\UserState;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
+use Symfony\Component\Validator\Constraints\DateTime;
+
+/**
+ * @extends ServiceEntityRepository<User>
+ *
+ * @method User|null find($id, $lockMode = null, $lockVersion = null)
+ * @method User|null findOneBy(array $criteria, array $orderBy = null)
+ * @method User[]    findAll()
+ * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
+
+    public function add(User $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(User $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    /**
+     * Used to upgrade (rehash) the user's password automatically over time.
+     */
+    public function upgradePassword(PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', \get_class($user)));
+        }
+
+        $user->setPassword($newHashedPassword);
+
+        $this->add($user, true);
+    }
+
+    public function checkUserValidation(User $user): UserState
+    {
+        $actualTime = new \DateTime('now');
+        $TimeTest = new \DateTime($user->getResetDate());
+        $diff = $TimeTest->diff($actualTime);
+
+        $state = new UserState();
+
+        switch(true) {
+            case !$user->isVerified():
+                $state->setState(UserState::ERROR);
+                $state->setMessage("L'utilisateur n'est pas activé");
+                return $state;
+                break;
+            case $user->getResetDate() && $diff->days > 0:
+                $state->setState(UserState::ERROR);
+                $state->setMessage("Le code de réinitialisation a déjà été envoyé il y a moins de 24h");
+                return $state;
+                break;
+        }
+
+        $state->setState(UserState::SUCCESS);
+        $state->setMessage("Mail de réinitialisation envoyé");
+
+        return $state;
+    }
+
+//    /**
+//     * @return User[] Returns an array of User objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('u')
+//            ->andWhere('u.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('u.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?User
+//    {
+//        return $this->createQueryBuilder('u')
+//            ->andWhere('u.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
